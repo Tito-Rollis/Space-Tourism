330aa0bcb5bc2eab3771843ed97bac720e37aab6
Tito-Rollis <ouvalres@gmail.com>
2022-01-12 09:10:50 +0700

    removing useeffect in crew

--- src/pages/crew/index.js
+++ src/pages/crew/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Nav from '../../components/nav/index';
 import style from './crew.module.css';
 import { data } from '../../api/data';
